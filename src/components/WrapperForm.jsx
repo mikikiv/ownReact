0014a0d976d01a23862d70b31d5a20d4f39350fe
Mikey <villavicem@gmail.com>
2022-08-31 21:44:23 -0700

    all the fields and yn's have state now

--- src/components/WrapperForm.jsx
+++ src/components/WrapperForm.jsx
@@ -1,27 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function WrapperForm({ handleSubmit }) {
+  const [url, setUrl] = useState('')
+  const [element, setElement] = useState('')
+  const [checked, setChecked] = useState(true)
+  const [title, setTitle] = useState('Search')
+
+  const handleUrlChange = (e) => {
+    setUrl(e.target.value)
+  }
+  const handleElementChange = (e) => {
+    setElement(e.target.value)
+  }
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value)
+  }
+  const handleCheck = (e) => {
+    setChecked(!checked)
+
+    if (!checked) {
+      e.target.value = true
+    } else {
+      e.target.value = false
+    }
+  }
+
+  let includeTitle = checked === true ? title : ''
+  let website = url + element + includeTitle
+
   return (
     <form>
       <div>
         <label htmlFor="url">Website</label>
-        <input type="url" name="url" id="url" />
+        <input id="url" onChange={handleUrlChange} type="url" value={url} />
       </div>
       <div>
         <label htmlFor="element">ID/Class/Element</label>
-        <input type="text" name="element" id="element" />
+        <input
+          id="element"
+          type="text"
+          onChange={handleElementChange}
+          value={element}
+        />
       </div>
       <label htmlFor="titleSwitch">Title Y/N</label>
-      <input type="checkbox" name="titleSwitch" id="titleSwitch" />
+      <input
+        id="titleyn"
+        type="checkbox"
+        checked={checked}
+        onChange={handleCheck}
+      />
+      <input
+        id="title"
+        type="text"
+        onChange={handleTitleChange}
+        placeholder="Search"
+      />
       <button
-        onClick={(e) =>
-          handleSubmit([localStorage.length + 1, localStorage.length + 1])
-        }
+        value="id"
+        onClick={(e) => handleSubmit([localStorage.length + 1, website])}
       >
         ID
       </button>
-      <button onClick={() => handleSubmit('class')}>Class</button>
-      <button onClick={() => handleSubmit('element')}>Element</button>
+      <button value="class" onClick={() => handleSubmit('class')}>
+        Class
+      </button>
+      <button value="element" onClick={() => handleSubmit('element')}>
+        Element
+      </button>
     </form>
   )
 }
